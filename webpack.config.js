a9f76fb4d8552129902b75828b590e8666b01f85
Jordan Boles <jordanboles1@gmail.com>
2019-10-07 14:13:26 -0600

    file structure built

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/react-client/src');
+var DIST_DIR = path.join(__dirname, '/react-client/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/app.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  module : {
+    rules : [
+      {
+        test: /\.(s*)css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test : /\.jsx?/,
+        include : SRC_DIR,
+        loader : 'babel-loader',      
+        query: {
+          presets: ["@babel/preset-env", "@babel/preset-react"]
+       },
+    }]
+  }
+};
