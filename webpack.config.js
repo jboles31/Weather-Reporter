deb695a1042b37c0ce987b01d6ac7a55a4b4d688
Jordan Boles <jordanboles1@gmail.com>
2019-10-17 16:50:17 -0600

    webpack config for image-loader

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,20 @@ module.exports = {
         query: {
           presets: ["@babel/preset-env", "@babel/preset-react"]
        },
-    }]
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ],
+      }
+    ]
   }
 };
